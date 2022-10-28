846abe6a66077e962bf7b0316c64e08d0b92bbc1
cha_jae_geon <jaegun920@naver.com>
2022-10-28 17:20:58 +0900

    express routes 예제 commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+
+const app = express();
+app.set('port', process.env.PORT || 5050);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+app.use ('/', indexRouter);
+app.use('/user',userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+});
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+    let port = app.get('port');
+    console.log(`http://localhost:${port} 에서 대기 중`);
+});

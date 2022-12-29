9c900a3c14cada943ac944cc5593bc98c4fe21ee
cha_jae_geon <jaegun920@naver.com>
2022-12-29 15:50:52 +0900

    static 미들웨어 추가

--- app.js
+++ app.js
@@ -1,57 +1,38 @@
 const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
 const dotenv = require('dotenv');
-const path = require('path');
+const morgan = require('morgan');
 
 dotenv.config();
-const indexRouter = require('./routes');
-const userRouter = require('./routes/user');
-
 const app = express();
-app.set('port', process.env.PORT || 5050);
+const port = process.env.PORT || 8888;
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
-    name: 'session-cookie',
-}));
 
-app.use ('/', indexRouter);
-app.use('/user',userRouter);
-
+/* 전처리 미들웨어 */
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');
+  console.log('모든 요청에 다 실행됩니다.');
+  next();
 });
 
-app.use((err, req, res, next) => {
-    console.log('모든 요청에 다 실행됩니다.');
-    next();
-});
-app.get('/', (req, res, next) => {
+/* 라우트 미들웨어 */
+app.get(
+  '/',
+  (req, res, next) => {
     console.log('GET / 요청에서만 실행됩니다.');
     next();
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
-});
+  },
+  (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+  }
+);
 
+/* 에러처리 미들웨어 */
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
-app.listen(app.get('port'), () => {
-    let port = app.get('port');
-    console.log(`http://localhost:${port} 에서 대기 중`);
+app.listen(port, () => {
+  console.log(`http://localhost:${port} 서버 대기중`);
 });

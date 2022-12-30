5d5f1cae0e7b96e992de1ab83db3acb2a3bef583
cha_jae_geon <jaegun920@naver.com>
2022-12-30 16:09:06 +0900

    login 기능 추가

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8888;
+const secret = process.env.PASSWORD || process.env.COOKIE_SECRET;
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -28,10 +29,17 @@ app.use(
 
 /* 로그인 미들웨어 */
 app.post('/login', (req, res) => {
-  let { username } = req.body;
-  req.session.user = { username };
-  // res.send('로그인 성공');
-  res.redirect('/');
+  let { username, password } = req.body;
+  /* 로그인 성공시 */
+  if ( password === secret ) {
+    req.session.user = { username };
+    res.redirect('/');
+  } else {
+  /* 로그인 실패시 */
+    res.redirect('/login.html');
+  }
+  
+  
 });
 
 /* 전처리 미들웨어 */
@@ -48,8 +56,13 @@ app.use((req, res, next) => {
 app.get(
   '/',
   (req, res, next) => {
-    console.log('GET / 요청에서만 실행됩니다.');
-    next();
+    let user = req.session.user;
+    if (user) {
+      /* 로그인 환영 메세지 */
+      res.send(`${user.username}님 환영합니다.`);
+    } else {
+      next();
+    }
   },
   (req, res) => {
     throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
